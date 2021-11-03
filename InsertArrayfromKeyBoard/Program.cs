69f223e6b6d465fa9af126631d7790d75e175544
Vasily Yanichkin <vas-yan2007@yandex.ru>
2021-11-03 14:31:42 +0300

    Program to fill the array from the keyboard added

---
+++ InsertArrayfromKeyBoard/Program.cs
@@ -0,0 +1,14 @@
+﻿// Ввод массива с клавиатуры
+int[] a; // описание массива
+a = new int [10]; // создание массива
+int i;
+
+for(int j = 0; j < 10; ++j) 
+{
+   string s = Console.ReadLine();
+   a[j]=Convert.ToInt32(s);
+}
+for(i = 0; i < 10; ++i) 
+{
+    Console.Write(a[i]+ " ");
+}
