9022f169e5d26c7d1f15ffb3779f30bfcad2fc47
Vasily Yanichkin <vas-yan2007@yandex.ru>
2021-11-03 13:47:33 +0300

    Prog Max out of 3 added

---
+++ Max3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Нахождение максимального из трех 
+
+int a = -70;
+int b = -200;
+int c = 45;
+
+int max = a;
+
+
+{
+    if (b>max) max = b;
+    if (c>max) max = c; 
+    
+}
+
+Console.WriteLine(max);
